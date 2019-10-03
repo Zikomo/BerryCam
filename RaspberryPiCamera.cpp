32c1b68c749870c9692ac254ece293ba58677b39
Zikomo Fields <zikomo.fields@gmail.com>
2019-10-02 23:21:45 -0500

    Adding hardware horizontal and vertical flip as well as 0, 90, 180, 270 rotations.

--- RaspberryPiCamera.cpp
+++ RaspberryPiCamera.cpp
@@ -19,7 +19,6 @@ BerryCam::RaspberryPiCamera::RaspberryPiCamera(std::shared_ptr<Encoder> encoder)
     _cameraVideoPort = _camera->output[MMAL_CAMERA_VIDEO_PORT];
     ptree defaultParameters;
     setCameraParameters(defaultParameters);
-
 }
 
 BerryCam::RaspberryPiCamera::~RaspberryPiCamera() = default;
@@ -30,7 +29,7 @@ void BerryCam::RaspberryPiCamera::start() {
             _cameraVideoPort->buffer_size);
     checkStatus(mmal_port_enable(_cameraVideoPort, onFrameReceivedStaticCallback), "mmal_port_enable");
     checkStatus(mmal_component_enable(_camera), "mmal_component_enable");
-    int queueLength = mmal_queue_length(_cameraVideoPortPool->queue);
+    unsigned int queueLength = mmal_queue_length(_cameraVideoPortPool->queue);
     for (int i = 0; i < queueLength; i++) {
         MMAL_BUFFER_HEADER_T *buffer = mmal_queue_get(_cameraVideoPortPool->queue);
         if (!buffer) {
@@ -64,13 +63,15 @@ void BerryCam::RaspberryPiCamera::setCameraParameters(ptree &cameraParameters) {
     };
     checkStatus(mmal_port_parameter_set(_camera->control, &camConfig.hdr), "mmal_port_parameter_set");
 
+    setFlip(cameraParameters);
+    setRotation(cameraParameters);
+
     auto width = cameraParameters.get<unsigned int>(CAMERA_PREVIEW_WIDTH);
     auto height = cameraParameters.get<unsigned int>(CAMERA_PREVIEW_HEIGHT);
 
     setVideoFormat(width, height);
     setCameraVideoPort(width, height);
     checkStatus(mmal_port_format_commit(_cameraVideoPort), "mmal_port_format_commit");
-
 }
 
 void BerryCam::RaspberryPiCamera::setCameraVideoPort(unsigned int width, unsigned int height)  {
@@ -109,7 +110,7 @@ ptree BerryCam::RaspberryPiCamera::getCameraParameters() {
     return parameters;
 }
 
-void BerryCam::RaspberryPiCamera::checkStatus(MMAL_STATUS_T status, std::string description = "") {
+void BerryCam::RaspberryPiCamera::checkStatus(MMAL_STATUS_T status, const std::string& description = "") {
     if (status != MMAL_SUCCESS) {
         throw std::runtime_error(description);
     }
@@ -138,6 +139,34 @@ void BerryCam::RaspberryPiCamera::onFrameReceived(unsigned char *frameData) {
     if ((_encoder == nullptr) || (frameData == nullptr))
         return;
     _encoder->encode(frameData);
+}
+
+void BerryCam::RaspberryPiCamera::setFlip(ptree &cameraParameters) {
+    MMAL_PARAMETER_MIRROR_T mirror = {{MMAL_PARAMETER_MIRROR, sizeof(MMAL_PARAMETER_MIRROR_T)}, MMAL_PARAM_MIRROR_NONE};
+
+    int horizontalFlip = Utilities::SafeGet<int>(cameraParameters, CAMERA_HORIZONTAL_FLIP, 0);
+    int verticalFlip = Utilities::SafeGet<int>(cameraParameters, CAMERA_VERTICAL_FLIP, 0);
+    
+    if (horizontalFlip && verticalFlip)
+        mirror.value = MMAL_PARAM_MIRROR_BOTH;
+    else if (horizontalFlip)
+        mirror.value = MMAL_PARAM_MIRROR_HORIZONTAL;
+    else if (verticalFlip)
+        mirror.value = MMAL_PARAM_MIRROR_VERTICAL;
+
+    checkStatus(mmal_port_parameter_set(_camera->output[0], &mirror.hdr));
+    checkStatus(mmal_port_parameter_set(_camera->output[1], &mirror.hdr));
+    checkStatus(mmal_port_parameter_set(_camera->output[2], &mirror.hdr));
+}
+
+void BerryCam::RaspberryPiCamera::setRotation(boost::property_tree::ptree &cameraParameters) {
+    int rotation = Utilities::SafeGet<int>(cameraParameters, CAMERA_ROTATION, 0);
+
+    int my_rotation = ((rotation % 360 ) / 90) * 90;
+
+    checkStatus(mmal_port_parameter_set_int32(_camera->output[0], MMAL_PARAMETER_ROTATION, my_rotation));
+    checkStatus(mmal_port_parameter_set_int32(_camera->output[1], MMAL_PARAMETER_ROTATION, my_rotation));
+    checkStatus(mmal_port_parameter_set_int32(_camera->output[2], MMAL_PARAMETER_ROTATION, my_rotation));
 }
 
 
